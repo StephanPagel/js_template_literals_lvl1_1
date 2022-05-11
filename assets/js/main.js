dca37b532974f4c52b52729325f78c2f6080597d
StephanPagel <stephan.pagel84@gmail.com>
2022-05-11 23:36:07 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+let Name = 'Benjamin';
+let Nachname = 'Blümchen';
+let Alter = 'steinalt';
+let Geburtsort = 'Neustadt';
+let Größe = '3.50m';
+let Gewicht = '3 Tonnen';
+let Hobbys = 'mit Otto spielen';
+let Computerspiele = 'Elefantenrennen 3';
+let Lieblingsessen = 'Zuckerstückchen';
+let Lieblingssport = 'Elefantenpolo';
+let Lieblingsmodemarke = 'alles, was mir passt';
+let Lieblingsjahreszeit = 'Sommer';
+let Lieblingsurlaubsort = 'Tansania';
+
+console.log(`Hallo, mein Name ist ${Name} ${Nachname}. Ich bin ${Alter} und wurde in ${Geburtsort} geboren. Ich bin ${Größe} groß und wiege ${Gewicht}. In meiner Freizeit gehe ich am liebsten ${Hobbys}. Oft spielen wir am Computer zusammen ${Computerspiele} und naschen dabei ${Lieblingsessen}. Außerdem spiele ich gern ${Lieblingssport}. Ich trage am liebsten ${Lieblingsmodemarke} und mache im ${Lieblingsjahreszeit} gern Urlaub in meiner Heimat ${Lieblingsurlaubsort}.`);
+
+txt = `Hallo, mein Name ist ${Name} ${Nachname}. Ich bin ${Alter} und wurde in ${Geburtsort} geboren. Ich bin ${Größe} groß und wiege ${Gewicht}. In meiner Freizeit gehe ich am liebsten ${Hobbys}. Oft spielen wir am Computer zusammen ${Computerspiele} und naschen dabei ${Lieblingsessen}. Außerdem spiele ich gern ${Lieblingssport}. Ich trage am liebsten ${Lieblingsmodemarke} und mache im ${Lieblingsjahreszeit} gern Urlaub in meiner Heimat ${Lieblingsurlaubsort}.`;
+document.write(txt + "<br>");
